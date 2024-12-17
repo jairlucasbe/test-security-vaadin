8b6fc66d20d9e05a40023aa12b48c1be05e07008
start.vaadin.com <no-reply@vaadin.com>
2024-12-17 16:33:05 +0000

    Generated project

---
+++ src/main/java/com/upgrade/example/data/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.upgrade.example.data;
+
+import org.springframework.lang.NonNullApi;
---
+++ src/main/java/com/upgrade/example/services/package-info.java
@@ -0,0 +1,4 @@
+@NonNullApi
+package com.upgrade.example.services;
+
+import org.springframework.lang.NonNullApi;
